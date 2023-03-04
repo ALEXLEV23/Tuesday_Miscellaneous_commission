1eeac5f651af929823e6a0d7094398446f9636d0
ALEX_ASA <aleksandr.asatiani@mail.ru>
2023-03-04 10:11:57 +0300

    Edit Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,62 +1,53 @@
-const val TYPE_1 = "Maestro"
-const val TYPE_2 = "MasterCard"
-const val MAXAMOUNT_TYPE_1 = 75000_00
-const val FIXCOMMISSION_TYPE_1 = 20_00
-const val COMMISSIONPERCENTAGE_TYPE_1 = 0.06
-const val TYPE_3 = "VISA"
-const val TYPE_4 = "Мир"
-const val FIXCOMMISSION_TYPE_3 = 35_00
-const val COMMISSIONPERCENTAGE_TYPE_3 = 0.0075
-const val TYPE_5 = "VK Pay"
-const val COMMISSIONPERCENTAGE_TYPE_5 = 0
-
 fun main() {
-    testCalcPrint(type = TYPE_1, amountTransfer = 100_00)
-    testCalcPrint(TYPE_1, 400_00, 100_00)
-    testCalcPrint(TYPE_1, 100_000_00, 50_00)
-    testCalcPrint(TYPE_2, 0, 100_00)
-    testCalcPrint(TYPE_2, 400_00, 100_00)
-    testCalcPrint(TYPE_2, 100_000_00, 50_00)
-    testCalcPrint(TYPE_3, 0, 100_00)
-    testCalcPrint(TYPE_3, 400_00, 1000_00)
-    testCalcPrint(TYPE_3, 100_000_00, 5000_00)
-    testCalcPrint(TYPE_4, 0, 100_00)
-    testCalcPrint(TYPE_4, 400_00, 1000_00)
-    testCalcPrint(TYPE_4, 100_000_00, 5000_00)
-    testCalcPrint(TYPE_5, 100_000_00, 5000_00)
-    testCalcPrint(amountOfPreviousPurchases = 100_000_00, amountTransfer = 15000_00)
-    testCalcPrint(amountTransfer = 25000_00)
+    countComission("Maestro", 80_000, 5_000, 1_000)
 }
 
-fun testCalcPrint(type : String = TYPE_5,
-                  amountOfPreviousPurchases : Int = 0,
-                  amountTransfer : Int){
-    println("=============================================================================================");
-    println("Сумма перевода: " + convertToRubKop(amountTransfer));
-    println("Тип карты: $type");
-    println("Сумма предыдущих переводов в этом месяце: " + convertToRubKop(amountOfPreviousPurchases));
-    println("Комиссия: " + convertToRubKop(calcCommission(type, amountOfPreviousPurchases, amountTransfer)));
-}
-
-fun convertToRubKop(amount : Int) : String{
-    return (amount / 100).toInt().toString() + " руб " + amount % 100 + " коп"
-}
-fun calcCommission(type : String,
-                   amountOfPreviousPurchases : Int,
-                   amountTransfer : Int) : Int{
-    when(type){
-        TYPE_1, TYPE_2 -> {
-            if ((amountOfPreviousPurchases + amountTransfer) in (1..MAXAMOUNT_TYPE_1)) return 0
-            else return ( FIXCOMMISSION_TYPE_1 + COMMISSIONPERCENTAGE_TYPE_1 * amountTransfer).toInt()
-        }
-        TYPE_3, TYPE_4 -> {
-            if (COMMISSIONPERCENTAGE_TYPE_3 * amountTransfer > FIXCOMMISSION_TYPE_1)
-                return (COMMISSIONPERCENTAGE_TYPE_3 * amountTransfer).toInt()
-            else return  FIXCOMMISSION_TYPE_3
-        }
-        TYPE_5 -> {
-            return (COMMISSIONPERCENTAGE_TYPE_5 * amountTransfer).toInt()
-        }
+fun countComission(
+    cardType: String = "Vk Pay",
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+    amount: Int
+) {
+    when (cardType) {
+        "Mastercard", "Maestro" -> countComissionMastercardMaestro(amount, totalAmountOfMonth, totalAmountOfDay)
+        "Visa", "Мир" -> countComissionVisaMir(amount, totalAmountOfMonth, totalAmountOfDay)
+        "Vk Pay" -> countComissionVkPay(totalAmountOfMonth, totalAmountOfDay)
     }
-    return 0
+}
+
+fun countComissionMastercardMaestro(
+    amount: Int,
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 150_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 600_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        totalAmountOfMonth > 75_000 -> println("Комиссия за перевод составит ${amount * 0.006 + 20.00} руб.")
+        else -> println("Комиссия за перевод отсутствует")
+    }
+}
+
+fun countComissionVisaMir(
+    amount: Int,
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 150_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 600_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        amount * 0.0075 < 35 -> println("Комиссия за перевод составит 35 руб.")
+        else -> println("Комиссия за перевод составит ${amount * 0.0075} руб.")
+    }
+}
+
+fun countComissionVkPay(
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 15_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 40_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        else -> println("Комиссия за перевод отсутствует")
+    }
 }
